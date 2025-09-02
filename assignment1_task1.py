f7b3d94fc2128c4db065d278adca9766df4e6b3c
amits0007 <amit3sati@gmail.com>
2025-09-02 22:40:40 +0530

    Add files via upload

---
+++ assignment1_task1.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the fist number: "))
+b=int(input("Enter the second number: "))
+print()
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("Addition :" ,add)
+print("Subtraction :",sub)
+print("Multiplication : ",mul)
+print("Division :",div)
---
+++ assignment1_task2.py
@@ -0,0 +1,3 @@
+f=input("Enter your first name : ")
+l=input("Enter your last name : ")
+print('hello,' ,f + l, 'Welcome to Python program.')
